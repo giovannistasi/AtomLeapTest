4e4dec16e2c6837c4d0dd24314f04dc84bc89e9d
giovannistasi <gsstasi@gmail.com>
2020-05-17 11:58:38 +0200

    feat: group data from API and sort in ascending order

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,33 @@
 import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 
-function Chart ({ fundingData }) {
+function Chart({ fundingData }) {
   useEffect(() => {
-    console.log(fundingData)
+    const nestedData = groupData(fundingData);
+    drawChart(nestedData);
   }, [fundingData])
+
+  // group the data by category and calculate the total funding
+  function groupData(data) {
+    const nestedData = d3.nest()
+      .key(d => d.category)
+      .rollup(v => {
+        return {
+          fundingAmount: d3.sum(v, d => d.fundingAmount),
+          fundingRounds: v
+        }
+      })
+      .entries(data)
+      .sort((a, b) => {
+        return a.value.fundingRounds.length - b.value.fundingRounds.length;
+      });
+    return nestedData;
+  }
+
+  function drawChart(nestedData) {
+    console.log(nestedData);
+  }
+
   return (
     <div>Hello</div>
   )
